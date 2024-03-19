a7fd8db6d09cc9f032a845aa14b8d1fe3b8a94ad
elfi <145452036+elifftorun@users.noreply.github.com>
2024-03-19 12:54:56 +0300

    Add files via upload

---
+++ filmArsivim/FormFilmler.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace filmArsivim
+{
+    partial class FormFilmler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvFilmler = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilmler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvFilmler
+            // 
+            this.dgvFilmler.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvFilmler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFilmler.Location = new System.Drawing.Point(25, 127);
+            this.dgvFilmler.Name = "dgvFilmler";
+            this.dgvFilmler.Size = new System.Drawing.Size(770, 358);
+            this.dgvFilmler.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(508, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(287, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "eleştiriler";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormFilmler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(835, 497);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvFilmler);
+            this.Name = "FormFilmler";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilmler)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvFilmler;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
